081cb9628fb0656ccf36280436bbfe731630de80
LaptevPavel <pal@alteos.com>
2020-11-12 12:27:40 +0100

    new start with glfx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,66 +1,89 @@
 import * as React from "react"
-import { fabric } from "fabric"
 import styles from "./app.module.scss"
+import fx from "glfx"
 //
 // import { imageToArrayBuffer } from "./utils"
 // import Range from "./components/Range"
 
+const drawCanvas = (c, ctx, source) => {
+    let hRatio = c.width / source.width
+    let vRatio = c.height / source.height
+    let ratio = Math.min(hRatio, vRatio)
+
+    ctx.drawImage(
+        source,
+        c.width / 2 - (source.width * ratio) / 2,
+        0,
+        source.width * ratio,
+        source.height * ratio
+    )
+}
+
 // Application
 const App = ({}) => {
-    const [canvas, setCanvas] = React.useState(null)
+    const [imageData, setImageData] = React.useState(null)
     const canvasRef = React.useRef(null)
-    // const [imageData, setImageData] = React.useState(null)
 
     React.useEffect(() => {
-        console.log(canvasRef.current)
-        const canvas = new fabric.Canvas("canvas", {
-            width: 300,
-            height: 160,
-            backgroundColor: "pink",
-        })
+        const c = canvasRef.current
+        c.width = 300 * 2
+        c.height = 150 * 2
+        const ctx = c.getContext("2d")
 
-        var rect = new fabric.Rect({
-            left: 100,
-            top: 100,
-            fill: "red",
-            width: 20,
-            height: 20,
-        })
+        const image = new Image()
 
-        // "add" rectangle onto canvas
-        canvas.add(rect)
-        // addRect(canvas)
+        image.onload = () => {
+            const fxc = fx.canvas()
+            fxc.width = 300
+            fxc.height = 150
+            const texture = fxc.texture(image)
+            // document.body.appendChild(fxc)
+            // ctx.drawImage(image, 0, 0)
+            fxc.draw(texture)
+                .triangleBlur(20)
+                .update()
+
+            ctx.drawImage(fxc, 0, 0)
+
+            // const c = canvasRef.current
+            // c.width = 300 * 2
+            // c.height = 150 * 2
+            // const ctx = c.getContext("2d")
+            // setImageData(image)
+            // drawCanvas(c, ctx, image)
+        }
+        image.src = imageData
+
+        onmessage = event => {
+            let imgData = event.data.pluginMessage.data
+
+            let base64Data =
+                "data:image/png;base64," +
+                btoa(
+                    new Uint8Array(imgData).reduce(function(data, byte) {
+                        return data + String.fromCharCode(byte)
+                    }, "")
+                )
+
+            setImageData(base64Data)
+        }
+
+        // let c = canvasRef.current
+        // c.width = 300 * 2
+        // c.height = 150 * 2
+        // let ctx = c.getContext("2d")
         // let img = new Image()
-        // img.src = imageData
+        // img.src = sourceImg
 
-        // img.onload = res => {
-        //     addImg(canvas, res)
-        // }
-
-        // onmessage = event => {
-        //     let imgData = event.data.pluginMessage.data
-
-        //     let base64Data =
-        //         "data:image/png;base64," +
-        //         btoa(
-        //             new Uint8Array(imgData).reduce(function(data, byte) {
-        //                 return data + String.fromCharCode(byte)
-        //             }, "")
-        //         )
-
-        //     setImageData(base64Data)
-        // }
-    }, [])
+        // img.onload = () => {}
+    }, [imageData])
 
     // RETURN
     return (
         <section className={styles.app}>
-            <canvas
-                ref={canvasRef}
-                id="canvas"
-                className={styles.previewSection}
-            />
+            <canvas ref={canvasRef} className={styles.previewSection} />
             <h2>Invert Channels</h2>
+
             <button>save settings</button>
         </section>
     )
